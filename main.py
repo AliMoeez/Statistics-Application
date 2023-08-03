1950bcb0e6c9973bf40a1fa7d50b1db31e2345ca
Moeez <oct30moeez@gmail.com>
2023-08-03 17:07:36 -0400

    commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt, numpy as np, tkinter, pandas as pd
+from sklearn import linear_model
+
+class Home:
+    pass
+
+class LinearRegression(Home):
+    pass
+
+class MultipleRegression(Home):
+    pass
+
+class TTest(Home):
+    pass
+
+class ArimaModel:
+    pass
+
+class Help(Home):
+    pass
+
