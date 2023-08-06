cc2263afd71b0c158e2ce51abef6fdc19cd33bed
Moeez <oct30moeez@gmail.com>
2023-08-06 13:01:25 -0400

    commit_1

--- main.py
+++ main.py
@@ -2,37 +2,48 @@ import matplotlib.pyplot as plt, numpy as np, pandas as pd
 from sklearn import linear_model
 from tkinter import *
 from tkinter import filedialog
+import functools
 
 SCREEN=Tk()
 
 SCREEN.geometry("1200x800") ; SCREEN.config(bg="gray0") ; SCREEN.title("Statistics Application") ; SCREEN.resizable(False,False)
 
+file_label=""
+
 class Home:
-    def __init__(self):
-        self.bg_colour="gray0" ; self.fg_colour="dodger blue" ; self.data_holder=[]
+    def __init__(self,file_label):
+        self.bg_colour="gray0" ; self.fg_colour="dodger blue" 
+
+        self.file_label=Label(SCREEN,text="You have uploaded PASS",bg="gray0",fg="gray0") 
+        self.file_label.configure(font=("Open Sans",10)) 
+        self.file_label.grid(column=1,row=3,ipady=20)
+
         self.title_label=Label(SCREEN,text="Statistics Application",bg=self.bg_colour,fg=self.fg_colour)  ; self.title_label.configure(font=("Open Sans",30))
         self.instrution_label=Label(SCREEN,text="Upload Your CSV/XLSX Files Below To Begin!",bg=self.bg_colour,fg=self.fg_colour) ; self.instrution_label.configure(font=("Open Sans",20))
         self.label_1=Label(SCREEN,text="",bg=self.bg_colour,fg=self.fg_colour)
 
-    def file_upload(self,file):
+    def file_upload(self):
         self.file=filedialog.askopenfilename(title="Upload CSV or XLSX Files",filetypes=(("CSV Files","*.csv"),("XLSX Files","*.xlxs")))
         self.file_label=Label(SCREEN,text=f"You have uploaded {self.file}",bg="gray0",fg="dodger blue") ; self.file_label.configure(font=("Open Sans",10)) ; self.file_label.grid(column=1,row=3,ipady=20)
-        self.data_holder.append(self.file)
-    
-    def data_organization(self):
-        if len(self.file[0])>0:
-            self.data=pd.read_csv(file)
-            print(self.data)
+
+    def data_organization(self): 
+        self.data=lambda:Home.file_upload(self)
 
     def text(self):
         self.title_label.grid(column=1,row=0,ipady=50) ; self.label_1.grid(column=0,row=0,ipadx=170) ; self.instrution_label.grid(column=1,row=1,ipady=20)
         
     def button(self):
-        self.upload_button=Button(SCREEN,text="Open File",command=lambda:Home.file_upload(self,file),bg=self.bg_colour,fg=self.fg_colour) ; self.upload_button.grid(column=1,row=2)
+        self.upload_button=Button(SCREEN,text="Open File",command=lambda:Home.file_upload(self),bg=self.bg_colour,fg=self.fg_colour) ; self.upload_button.grid(column=1,row=2)
+        print(self.file_label.cget("text"))
+
+    def confirmation_button(self):
+        self.upload_button=Button(SCREEN,text="Confirm File",command=lambda:Home.button(self),bg=self.bg_colour,fg=self.fg_colour) ; self.upload_button.grid(column=1,row=4)
+        
     
-home=Home()
+home=Home(file_label)
 home.text()
 home.button()
-home.data_organization(
+home.confirmation_button()
+home.data_organization()
 
 SCREEN.mainloop()
