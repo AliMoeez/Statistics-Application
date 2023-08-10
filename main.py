fd9e07733e206d558b3244ac8cc48335bc5dcec5
Moeez <oct30moeez@gmail.com>
2023-08-10 18:05:21 -0400

    commit_1

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ SCREEN=Tk()
 
 SCREEN.geometry("1200x800") ; SCREEN.config(bg="gray0") ; SCREEN.title("Statistics Application") ; SCREEN.resizable(False,False)
 
-file_label="" ; data="" ; data_label=""
+file_label="" ; data="" ; data_label="" ; string="TEST"
 
 class Home:
-    def __init__(self,file_label,data,data_label):
+    def __init__(self,file_label,data,data_label,string):
         self.bg_colour="gray0" ; self.fg_colour="dodger blue"  ; self.file_label=Label(SCREEN,text="You have uploaded PASS",bg="gray0",fg="gray0") 
         self.file_label.configure(font=("Open Sans",10))  ; self.file_label.grid(column=1,row=3,ipady=20)
         self.title_label=Label(SCREEN,text="Statistics Application",bg=self.bg_colour,fg=self.fg_colour)  ; self.title_label.configure(font=("Open Sans",30))
@@ -33,32 +33,39 @@ class Home:
         if self.file_text.endswith(".csv") or self.file_text.endswith(".xlsx"): self.data_label=Label(SCREEN,text=self.data.head(),bg=self.bg_colour,fg=self.fg_colour)
         else: self.data_label=Label(SCREEN,text=self.data,bg=self.bg_colour,fg=self.fg_colour)
         self.data_label.grid(column=1,row=6,pady=20)
+        
 
     def confirmation_button(self):
         self.upload_button=Button(SCREEN,text="Confirm File",command=lambda:Home.data_organization(self),bg=self.bg_colour,fg=self.fg_colour) ; self.upload_button.grid(column=1,row=4,pady=20)
-   
+        self.file_text=self.file_label.cget("text")[18:]
+        if (self.file_text.endswith(".csv") or self.file_text.endswith(".xlsx")) and self.string.get()=="Linear Regression":
+           print("HERE")
+
     def data_clear(self):
         self.data_label.destroy() ; self.file_label.destroy()
     
     def data_clear_button(self):
         self.clear_button=Button(SCREEN,text="Clear File",command=lambda:Home.data_clear(self),bg=self.bg_colour,fg=self.fg_colour) ; self.clear_button.grid(column=1,row=5)
 
-    def data_inputs(self):
-        entry_box_one=Entry(SCREEN) ; entry_box_one.config(bg=self.bg_colour,fg=self.fg_colour) ; entry_box_one.grid(column=1,row=9,sticky="W")
-        entry_box_two=Entry(SCREEN) ; entry_box_two.config(bg=self.bg_colour,fg=self.fg_colour) ; entry_box_two.grid(column=1,row=9,sticky="E")
-
     def test_type(self):
         self.test_label=Label(SCREEN,text="Choose The Test You Want To Use",bg=self.bg_colour,fg=self.fg_colour) ; self.test_label.grid(column=1,row=8,pady=20)
-        self.dropdown_test_options=["Linear Regression","Multiple Regression","T-Test","ARIMA"] ; self.string=StringVar()
+        self.dropdown_test_options=["Linear Regression","Multiple Regression","T-Test","ARIMA"] ; self.string=StringVar() 
         self.test_dropdown=OptionMenu(SCREEN,self.string,*self.dropdown_test_options) ; self.test_dropdown.config(bg=self.bg_colour,fg=self.fg_colour) 
         self.test_dropdown["menu"].config(bg=self.bg_colour,fg=self.fg_colour) ; self.test_dropdown.grid(column=1,row=9)
+        self.string.set(self.dropdown_test_options[0])
+
+    def next_step(self):
+        self.next_button=Button(SCREEN,text="Next",bg=self.bg_colour,fg=self.fg_colour,command=lambda:Home.confirmation_button(self)) ; self.next_button.grid(column=1,row=10,pady=20)
+
         
-home=Home(file_label,data,data_label)
+
+        
+home=Home(file_label,data,data_label,string)
 home.text()
 home.data_organization()
-home.confirmation_button()
-home.data_inputs()
 home.test_type()
+home.confirmation_button()
 home.data_clear_button()
+home.next_step()
 
 SCREEN.mainloop()
